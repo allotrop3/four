cacb69ac598fc25b331a3f6a73950fdcf2d14329
Jason Petersen <jasontpetersen@yahoo.com>
2015-07-18 00:27:54 +0100

    Add Attribute bind function

--- src/scripts/Attribute.js
+++ src/scripts/Attribute.js
@@ -5,11 +5,20 @@ var gl = require('./gl');
 
 class Attribute extends Entity
 {
-   constructor(program, attribute, name = 'attribute')
+   constructor(program, attribute, length, format = 'FLOAT', name = 'attribute')
    {
       super(name);
 
       this.location = gl.getAttributeLocation(program, attribute);
+      this.length = length;
+      this.format = gl[format];
+   }
+
+   bind(stride, offset)
+   {
+      this.enable();
+
+      gl.vertexAttribPointer(this.location, this.length, this.format, false, stride, offset);
    }
 
    enable()
