1bc379b66dec146d0ee06a84aceb12ae08627163
Jason Petersen <jasontpetersen@yahoo.com>
2015-07-19 18:56:22 +0100

    Add DirectionalLight docs

--- src/scripts/DirectionalLight.js
+++ src/scripts/DirectionalLight.js
@@ -2,15 +2,40 @@
 
 let Light = require('./Light');
 
+/**
+ * DirectionalLight is a directional light source to illuminate the scene
+ * @class DirectionalLight
+ * @name DirectionalLight
+ * @extends Light
+ * @param {string} [name=directional.light] - Instance name
+ * @param {vec3} ambient - Ambient component
+ * @param {vec3} diffuse - Diffuse component
+ * @param {vec3} specular - Specular component
+ * @param {number} [intensity=1] - Light intensity
+ * @param {vec3} [location=[0, 0, 0]] - Light position
+ * @param {vec3} [direction=[0, 0, 0]] - Light direction
+ */
 class DirectionalLight extends Light
 {
-   constructor(name = 'directional.light', ambient, diffuse, specular, intensity, location, direction)
+   constructor(name = 'directional.light', ambient, diffuse, specular, intensity, location, direction = [0, 0, 0])
    {
       super(name, ambient, diffuse, specular, intensity, location);
 
+      /**
+       * Light direction
+       * @var {vec3} DirectionalLight.direction
+       * @default [0, 0, 0]
+       * @private
+       */
       this.direction = direction;
    }
 
+   /**
+    * Set directional light shader uniform values
+    * @function DirectionalLight.bind
+    * @param {object} uniform - Directional light shader uniforms
+    * @returns {undefined}
+    */
    bind(uniform)
    {
       super.bind(uniform);
--- src/scripts/Light.js
+++ src/scripts/Light.js
@@ -2,9 +2,21 @@
 
 let Entity = require('./Entity');
 
+/**
+ * Light is an global light source to illuminate the scene
+ * @class Light
+ * @name Light
+ * @extends Entity
+ * @param {string} [name=directional.light] - Instance name
+ * @param {vec3} ambient - Ambient component
+ * @param {vec3} diffuse - Diffuse component
+ * @param {vec3} specular - Specular component
+ * @param {number} [intensity=1] - Light intensity
+ * @param {vec3} [location=[0, 0, 0]] - Light position
+ */
 class Light extends Entity
 {
-   constructor(name = 'light', ambient, diffuse, specular, intensity = 1, location)
+   constructor(name = 'light', ambient, diffuse, specular, intensity = 1, location = [0, 0, 0])
    {
       super(name);
 
