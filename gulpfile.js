c41fa5958d070113b979214d5197b2c8bce57834
Jason Petersen <jasontpetersen@yahoo.com>
2016-02-21 23:28:01 +0000

    Cleanup gulp file from old docs builder

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,6 @@ var webpack = require('webpack');
 var webpackStream = require('webpack-stream');
 var config = require('./webpack.config.js');
 var webserver = require('gulp-webserver');
-var ext = require('gulp-ext-replace');
-var rimraf = require('gulp-rimraf');
-var sequence = require('run-sequence');
-var pygmentize = require('pygmentize-bundled');
-var markdown = require('gulp-markdown');
-var marked = markdown.marked;
-var renderer = new marked.Renderer();
-var rendering = require('./renderer.config.js');
-var prettify = require('gulp-prettify');
 
 /**
  * gulp js
@@ -25,79 +16,6 @@ gulp.task('js', function()
    return gulp.src('src/four.js')
       .pipe(webpackStream(config.js, webpack))
       .pipe(gulp.dest('dist'));
-});
-
-/**
- * gulp md
- *
- * Compile markdown wiki to html.
- */
-gulp.task('md', function()
-{
-   marked.setOptions({
-      highlight: function(code, lang, cb)
-      {
-         return pygmentize(
-            {
-               lang: lang,
-               format: 'html'
-            },
-            code,
-            function(error, result)
-            {
-               cb(error, result.toString());
-            }
-         )
-      }
-   });
-
-   for (var tag in rendering)
-   {
-      if (tag in renderer)
-      {
-         renderer[tag] = rendering[tag];
-      }
-   }
-
-   gulp.src('projects/four.wiki/**/*.md')
-      .pipe(markdown(
-         {
-            renderer: renderer
-         }
-      ))
-      .pipe(gulp.dest('.api'))
-      .pipe(prettify(
-         {
-            indent_size: 4
-         }
-      ))
-      .pipe(gulp.dest('.api'))
-      .pipe(ext('.php'))
-      .pipe(gulp.dest('projects/fourjs.io/api'));
-});
-
-/**
- * gulp rimraf
- *
- * Remove .docs folder.
- */
-gulp.task('rimraf', function(cb)
-{
-   return gulp.src(['.api', 'projects/fourjs.io/api/_*.*'],
-      {
-         read: false
-      })
-      .pipe(rimraf());
-});
-
-/**
- * gulp docs
- *
- * Compile the documentation.
- */
-gulp.task('api', function(cb)
-{
-   sequence('md', 'rimraf', cb);
 });
 
 /**
